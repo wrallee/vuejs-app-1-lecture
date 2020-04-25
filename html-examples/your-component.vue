e93a11a6118e164247164acf40ad997d875d7af4
wrallee <wrallee@naver.com>
2020-04-25 22:37:44 +0900

    commit html-examples

---
+++ html-examples/your-component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span>
+      <button>{{ message }}</button>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data: {
+    message: 'click this button'
+  }
+}
+</script>
+
+<style lang="css">
+  span {
+    font-size: 1.2em;
+  }
+</style>
